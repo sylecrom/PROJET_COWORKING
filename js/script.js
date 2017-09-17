8242bade929bf8b466aa5a331dd463267f294214
Jeremy Perrin <jeremy.perrin09@gmail.com>
2017-09-17 18:40:44 +0200

    Evolution footer plus ajout bouton raccourcis

--- js/script.js
+++ js/script.js
@@ -53,4 +53,20 @@ $(function(){
             })
 
 });
-
+/*Bouton raccourcis*/
+window.onload = function () {
+  
+      var element = document.getElementById('cn-button');
+  
+      element.onclick = function () {
+          var d = document.getElementById("cn-wrapper").classList;
+          if (d.contains("opened-nav")) {
+              
+                 d.remove("opened-nav");
+              
+              } else {
+              
+                 d.add("opened-nav");
+              }
+      }
+  }
