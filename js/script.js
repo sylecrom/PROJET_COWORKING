e06838fcb3661989918b1f5c0b3a9dc2cce9609c
Sylvain LE CROM <sylvain.lecrom@free.fr>
2017-09-21 10:53:53 +0200

    modification debrief

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function initMap() {
   });
   var icons = {
     point: {
-      icon: 'poop.png',
+      icon: 'css/img/logomap.png',
     }
   };
   var features = [
@@ -131,20 +131,4 @@ $(function () {
   $(".serBanM").click(function () {
     $(".effetHi1").show();
   })
-});
-
-/** apparition des sections à l'écran **/
-$(document).ready(function () {
-  $('.apparition').viewportChecker({
-    // Class to add to the elements when they are visible
-    classToAdd: 'visible',
-    // The offset of the elements (let them appear earlier or later)
-    offset: 100,
-
-    // Add the possibility to remove the class if the elements are not visible
-    repeat: false,
-
-    // Callback to do after a class was added to an element. Action will return "add" or "remove", depending if the class was added or removed
-    callbackFunction: function (elem, action) { }
-  });
 });
